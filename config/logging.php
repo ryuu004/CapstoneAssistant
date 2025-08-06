4a895d5ed031f806c01c8c7cec106b4445f7d0eb
rhyio <rhyiotahari@gmail.com>
2025-08-06 10:55:56 +0800

    c

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('APP_ENV') === 'production' ? 'stderr' : env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
